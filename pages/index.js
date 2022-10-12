a14958fc5a674ccd281a109da01b4b331588ba49
HyperLabStat <114814641+HyperLabStat@users.noreply.github.com>
2022-10-12 16:51:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Marcolongo Digital Intelligence Test</code>
+          Marcolongo Digital Intelligence Test </code>
         </p>
       </main>
 
